bef1f8c455093801d779863f432c8db1eaff6ae9
Fahimul Bari's Studio <fahimulbari247@gmail.com>
2023-05-03 01:48:06 +0600

    Data-Layer Implemented (User, Transaction, Rating)

---
+++ DataLayer/Migrations/202305021945522_Rating&&Migration.cs
@@ -0,0 +1,37 @@
+﻿namespace DataLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RatingMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Ratings",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Value = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Transactions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Price = c.Single(nullable: false),
+                        Time = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Transactions");
+            DropTable("dbo.Ratings");
+        }
+    }
+}
