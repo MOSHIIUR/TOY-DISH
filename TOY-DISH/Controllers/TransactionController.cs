b74ada93623e06cb8a7f71e8d36e6e9b7da9969a
Moshiur Rahman <62719277+MOSHIIUR@users.noreply.github.com>
2023-05-13 21:26:06 +0600

    Fahim (#1)
    * Data-Layer Implemented (User, Transaction, Rating)
    
    * DAL & APP Layer Implemented (User, Transaction & Rating)
    
    * Minor
    
    * hopefully-conflict-resolvegit add .!
    
    ---------
    
    Co-authored-by: Fahimul Bari's Studio <fahimulbari247@gmail.com>

---
+++ TOY-DISH/Controllers/TransactionController.cs
@@ -0,0 +1,94 @@
+﻿using BusinessLayer.DTOs;
+using BusinessLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TOY_DISH.Controllers
+{
+    public class TransactionController : ApiController
+    {
+        [HttpGet]
+        [Route("api/transactions")]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                var data = TransactionService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+
+            }
+        }
+
+        [HttpPost]
+        [Route("api/transactions/add")]
+        public HttpResponseMessage Add(TransactionDTO data)
+        {
+            try
+            {
+                var res = TransactionService.Create(data);
+                if (res)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Inserted", Data = data });
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = "Not Inserted", Data = data });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = ex.Message, Data = data });
+            }
+        }
+
+        [HttpGet]
+        [Route("api/transactions/{id}")]
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+
+                return Request.CreateResponse(HttpStatusCode.OK, TransactionService.Get(id));
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/transactions/Delete/{id}")]
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                var res = TransactionService.Delete(id);
+                if (res)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Deleted", Data = id });
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = "Not Deleted", Data = id });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = ex.Message, Data = id });
+            }
+        }
+    }
+}
